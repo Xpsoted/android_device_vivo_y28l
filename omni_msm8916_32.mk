e1730175308d3168dc26e435c3a07e04c27395e0
Xpsoted <37337416+Xpsoted@users.noreply.github.com>
2022-05-26 21:16:59 +0800

    Add files via upload

---
+++ omni_msm8916_32.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from msm8916_32 device
+$(call inherit-product, device/vivo/msm8916_32/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := msm8916_32
+PRODUCT_NAME := omni_msm8916_32
+PRODUCT_BRAND := qcom
+PRODUCT_MODEL := vivo Y13L
+PRODUCT_MANUFACTURER := vivo
+PRODUCT_RELEASE_NAME := qcom vivo Y13L
